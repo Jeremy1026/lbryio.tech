22f642c787e8753b87e611afbed286dff8e14c23
Jeremy Curcio <jeremy1026@gmail.com>
2018-02-06 19:48:22 -0500

    Update home button link to reflect new route name

--- view/template/page/home.php
+++ view/template/page/home.php
@@ -3,7 +3,7 @@
 <main class="home">
   <div class="cover cover-light content content-light content-wide home__copy">
     <div class="spacer2 text-center">
-      <a href="/start" class="btn-primary btn-large spacer1">Get Learning</a><BR>
+      <a href="/learn" class="btn-primary btn-large spacer1">Get Learning</a><BR>
     </div>
   </div>
 </main>
